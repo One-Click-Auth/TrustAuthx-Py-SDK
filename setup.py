d7c54c99e059f92bca141b2f8d46adc9247fdb11
moonlightnexus <68702919+moonlightnexus@users.noreply.github.com>
2023-09-18 04:33:50 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='trustauthx',
-    version='0.2.0',
+    version='0.2.1',
     description='Official connector SDK for TrustAuthx',
     long_description='https://docs.trustauthx.com',
     author='moonlightnexus',
